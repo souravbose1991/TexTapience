aaabe2ecb7cedf7b339e5929e256b4841e0aac4d
Sourav Bose <36502353+souravbose1991@users.noreply.github.com>
2018-08-22 03:05:22 +0530

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,8 +1,8 @@
 #' @export
 runApp <- function() {
-  appDir <- system.file("app", package = "topicApp")
+  appDir <- system.file("app", package = "TexTrove")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `topicApp`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `TexTrove`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
